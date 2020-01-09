eb8c5a7d0de251050a971e39327b68dc11a9cb6c
Himank Pathak <himankpathak@gmail.com>
2020-01-09 18:13:36 +0530

    added about page with navigation

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
+import Link from "next/link";
+
 const Index = () => {
   return (
     <div>
+      <Link href="/about">
+        <a>About Page</a>
+      </Link>
       <p>Hello next.js</p>
     </div>
   );
